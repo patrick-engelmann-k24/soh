29fb9b0efdbd5660624ddc759a0cb038b4b9cfd5
Andreas Zill <andreas.zill.extern@kfzteile24.de>
2020-09-22 00:20:26 +0200

    Update url name for healthCheck

--- src/main/java/de/kfzteile24/salesOrderHub/controller/HealthCheck.java
+++ src/main/java/de/kfzteile24/salesOrderHub/controller/HealthCheck.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HealthCheck {
 
-    @GetMapping("/healthcheck")
+    @GetMapping("/healthCheck")
     public ResponseEntity<String> healthResponse() {
         return new ResponseEntity<>("OK", HttpStatus.OK);
 
