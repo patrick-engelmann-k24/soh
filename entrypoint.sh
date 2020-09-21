95f42627d16ac9a03012ad24eb199eeb1d3fa54a
Andreas Zill <andreas.zill.extern@kfzteile24.de>
2020-09-21 17:03:12 +0200

    Update Dockerfile for issues #3

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -eu # exit on first error, treat substitution of unset env vars as errors
 
 export FLYWAY_URL="jdbc:postgresql://${soh_db_host}:${soh_db_port}/${soh_db_database}"
