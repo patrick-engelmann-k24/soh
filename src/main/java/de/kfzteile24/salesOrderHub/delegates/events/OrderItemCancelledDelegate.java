5fec2c3753693d339611d459bc3d3696307a71c4
Andreas Zill <andreas.zill.extern@kfzteile24.de>
2020-10-06 09:44:38 +0200

    [DEV-15938,DEV-15939] Add delegates for throw events in order item sub process

---
+++ src/main/java/de/kfzteile24/salesOrderHub/delegates/events/OrderItemCancelledDelegate.java
@@ -0,0 +1,15 @@
+package de.kfzteile24.salesOrderHub.delegates.events;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderItemCancelledDelegate implements JavaDelegate {
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+
+    }
+
+}
