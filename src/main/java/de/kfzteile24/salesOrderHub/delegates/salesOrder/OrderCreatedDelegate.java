c17da370a84a26862f8c68814f6b05330180c910
Andreas Zill <andreas.zill.extern@kfzteile24.de>
2020-10-11 23:48:41 +0200

    [DEV-15934] add code to publish original sales order from database

--- src/main/java/de/kfzteile24/salesOrderHub/delegates/salesOrder/OrderCreatedDelegate.java
+++ src/main/java/de/kfzteile24/salesOrderHub/delegates/salesOrder/OrderCreatedDelegate.java
@@ -1,5 +1,9 @@
 package de.kfzteile24.salesOrderHub.delegates.salesOrder;
 
+import de.kfzteile24.salesOrderHub.constants.BPMSalesOrderProcess;
+import de.kfzteile24.salesOrderHub.constants.BpmItem;
+import de.kfzteile24.salesOrderHub.domain.SalesOrder;
+import de.kfzteile24.salesOrderHub.services.SalesOrderService;
 import de.kfzteile24.salesOrderHub.services.SnsPublishService;
 import lombok.extern.java.Log;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
@@ -7,6 +11,8 @@ import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 @Log
@@ -18,9 +24,20 @@ public class OrderCreatedDelegate implements JavaDelegate {
     @Value("${soh.sns.topic.orderCreated}")
     String snsOrderCreatedTopic; //= "arn:aws:sns:eu-central-1:000000000000:soh-order-created";
 
+    @Autowired
+    SalesOrderService salesOrderService;
+
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
         log.info("SNS-Topic: " + snsOrderCreatedTopic);
-        snsPublishService.send(snsOrderCreatedTopic, "subject", "Body");
+
+        String orderNumber = (String) delegateExecution.getVariable(BPMSalesOrderProcess.VAR_ORDER_ID.getName());
+        Optional<SalesOrder> salesOrderOptional = salesOrderService.getOrderByOrderNumber(orderNumber);
+        if (salesOrderOptional.isPresent()) {
+            SalesOrder salesOrder = salesOrderOptional.get();
+            snsPublishService.send(snsOrderCreatedTopic, "Sales Order Created", salesOrder.getOriginalOrder());
+        } else {
+            throw new Exception("no sales order found");
+        }
     }
 }
