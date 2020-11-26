ca4ce8f11241711aee56abec890511b894c79030
Andreas Zill <andreas.zill.extern@kfzteile24.de>
2020-11-26 15:14:34 +0100

    [DEV-14257] improve code with new camunda helper

--- src/main/java/de/kfzteile24/salesOrderHub/services/SqsReceiveService.java
+++ src/main/java/de/kfzteile24/salesOrderHub/services/SqsReceiveService.java
@@ -2,22 +2,19 @@ package de.kfzteile24.salesOrderHub.services;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
-import de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.Events;
 import de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.Messages;
 import de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.Variables;
+import de.kfzteile24.salesOrderHub.delegates.helper.CamundaHelper;
 import de.kfzteile24.salesOrderHub.domain.SalesOrder;
 import de.kfzteile24.salesOrderHub.dto.OrderJSON;
-import de.kfzteile24.salesOrderHub.dto.sqs.EcpOrder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.runtime.MessageCorrelationResult;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
-
-import javax.validation.constraints.NotNull;
 
 @Service
 @Slf4j
@@ -27,6 +24,7 @@ public class SqsReceiveService {
     final Gson gson;
     final RuntimeService runtimeService;
     final SalesOrderService salesOrderService;
+    final CamundaHelper camundaHelper;
 
     @SqsListener("${soh.sqs.queue.ecpShopOrders}")
     public void queueListenerEcpShopOrders(String message, @Header("SenderId") String senderId) {
@@ -37,18 +35,15 @@ public class SqsReceiveService {
             final SalesOrder ecpSalesOrder = de.kfzteile24.salesOrderHub.domain.SalesOrder.builder()
                     .orderNumber(orderJSON.getOrderHeader().getOrderNumber())
                     .salesLocale(orderJSON.getOrderHeader().getOrigin().getLocale())
-                    .originalOrder(gson.toJson(orderJSON, OrderJSON.class))
+                    .originalOrder(orderJSON)
                     .build();
 
             salesOrderService.save(ecpSalesOrder);
 
-            MessageCorrelationResult result = runtimeService.createMessageCorrelation(Messages.ORDER_RECEIVED_ECP.getName())
-                    .processInstanceBusinessKey(ecpSalesOrder.getOrderNumber())
-                    .setVariable(Variables.ORDER_NUMBER.getName(), ecpSalesOrder.getOrderNumber())
-                    .correlateWithResult();
+            ProcessInstance result = camundaHelper.createOrderProcess(ecpSalesOrder, Messages.ORDER_RECEIVED_ECP);
 
-            if (result.getProcessInstance() != null) {
-                log.info("New ecp order process started: " + result.getProcessInstance().getId());
+            if (result != null) {
+                log.info("New ecp order process started: " + result.getId());
             }
         } catch (JsonSyntaxException e) {
             log.error("ECP Order could not parsed from Json");
