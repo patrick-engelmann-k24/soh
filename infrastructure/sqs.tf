3b85e7a4e40f76664b7026eb2d54905d1cf02590
patrick engelmann <patrick.engelmann@kfzteile24.de>
2020-12-11 11:13:42 +0100

    excluded sns ecp subscription and added dlqs for service queues

--- infrastructure/sqs.tf
+++ infrastructure/sqs.tf
@@ -11,30 +11,65 @@ locals {
   }
 }
 
+resource "aws_sqs_queue" "ecp_shop_orders_dlq" {
+  name = local.sqs_queues.ecp_shop_orders
+}
+
 resource "aws_sqs_queue" "ecp_shop_orders" {
   name = local.sqs_queues.ecp_shop_orders
+  redrive_policy  = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.ecp_shop_orders_dlq.arn}\",\"maxReceiveCount\":4}"
+}
+
+resource "aws_sqs_queue" "soh_order_item_shipped_dlq" {
+  name = local.sqs_queues.order_item_shipped
 }
 
 resource "aws_sqs_queue" "soh_order_item_shipped" {
   name = local.sqs_queues.order_item_shipped
+  redrive_policy  = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.soh_order_item_shipped_dlq.arn}\",\"maxReceiveCount\":4}"
+}
+
+resource "aws_sqs_queue" "soh_order_payment_secured_dlq" {
+  name = local.sqs_queues.order_payment_secured
 }
 
 resource "aws_sqs_queue" "soh_order_payment_secured" {
   name = local.sqs_queues.order_payment_secured
+  redrive_policy  = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.soh_order_payment_secured_dlq.arn}\",\"maxReceiveCount\":4}"
+}
+
+resource "aws_sqs_queue" "soh_order_item_transmitted_to_logistic_dlq" {
+  name = local.sqs_queues.order_item_transmitted_to_logistic
 }
 
 resource "aws_sqs_queue" "soh_order_item_transmitted_to_logistic" {
   name = local.sqs_queues.order_item_transmitted_to_logistic
+  redrive_policy  = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.soh_order_item_transmitted_to_logistic_dlq.arn}\",\"maxReceiveCount\":4}"
+}
+
+resource "aws_sqs_queue" "soh_order_item_packing_started_dlq" {
+  name = local.sqs_queues.order_item_packing_started
 }
 
 resource "aws_sqs_queue" "soh_order_item_packing_started" {
   name = local.sqs_queues.order_item_packing_started
+  redrive_policy  = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.soh_order_item_packing_started_dlq.arn}\",\"maxReceiveCount\":4}"
+}
+
+resource "aws_sqs_queue" "soh_order_item_tracking_id_received_dlq" {
+  name = local.sqs_queues.order_item_tracking_id_received
 }
 
 resource "aws_sqs_queue" "soh_order_item_tracking_id_received" {
   name = local.sqs_queues.order_item_tracking_id_received
+  redrive_policy  = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.soh_order_item_tracking_id_received_dlq.arn}\",\"maxReceiveCount\":4}"
+}
+
+resource "aws_sqs_queue" "soh_order_item_tour_started_dlq" {
+  name = local.sqs_queues.order_item_tour_started
 }
 
 resource "aws_sqs_queue" "soh_order_item_tour_started" {
   name = local.sqs_queues.order_item_tour_started
+  redrive_policy  = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.soh_order_item_tour_started_dlq.arn}\",\"maxReceiveCount\":4}"
 }
