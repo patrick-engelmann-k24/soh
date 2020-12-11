9834501197ab8c6dbe7e3bb4d7f928ba80b157f3
Andreas Zill <andreas.zill.extern@kfzteile24.de>
2020-12-11 17:49:00 +0100

    Correct dlq configuration

--- infrastructure/sqs.tf
+++ infrastructure/sqs.tf
@@ -2,17 +2,24 @@ locals {
   version = "v1"
   sqs_queues = {
     ecp_shop_orders = "${var.environment}-${local.service_prefix}-ecp-shop-orders-${local.version}",
+    ecp_shop_orders_dlq = "${var.environment}-${local.service_prefix}-ecp-shop-orders-${local.version}-dlq",
     order_item_shipped = "${var.environment}-${local.service_prefix}-order-item-shipped-${local.version}",
+    order_item_shipped_dlq = "${var.environment}-${local.service_prefix}-order-item-shipped-${local.version}-dlq",
     order_payment_secured = "${var.environment}-${local.service_prefix}-order-payment-secured-${local.version}"
+    order_payment_secured_dlq = "${var.environment}-${local.service_prefix}-order-payment-secured-${local.version}-dlq"
     order_item_transmitted_to_logistic = "${var.environment}-${local.service_prefix}-order-item-transmitted-to-logistic-${local.version}"
+    order_item_transmitted_to_logistic_dlq = "${var.environment}-${local.service_prefix}-order-item-transmitted-to-logistic-${local.version}-dlq"
     order_item_packing_started = "${var.environment}-${local.service_prefix}-order-item-packing-started-${local.version}"
+    order_item_packing_started_dlq = "${var.environment}-${local.service_prefix}-order-item-packing-started-${local.version}-dlq"
     order_item_tracking_id_received = "${var.environment}-${local.service_prefix}-order-item-tracking-id-received-${local.version}"
+    order_item_tracking_id_received_dlq = "${var.environment}-${local.service_prefix}-order-item-tracking-id-received-${local.version}-dlq"
     order_item_tour_started = "${var.environment}-${local.service_prefix}-order-item-tour-started-${local.version}"
+    order_item_tour_started_dlq = "${var.environment}-${local.service_prefix}-order-item-tour-started-${local.version}-dlq"
   }
 }
 
 resource "aws_sqs_queue" "ecp_shop_orders_dlq" {
-  name = local.sqs_queues.ecp_shop_orders
+  name = local.sqs_queues.ecp_shop_orders_dlq
 }
 
 resource "aws_sqs_queue" "ecp_shop_orders" {
@@ -21,7 +28,7 @@ resource "aws_sqs_queue" "ecp_shop_orders" {
 }
 
 resource "aws_sqs_queue" "soh_order_item_shipped_dlq" {
-  name = local.sqs_queues.order_item_shipped
+  name = local.sqs_queues.order_item_shipped_dlq
 }
 
 resource "aws_sqs_queue" "soh_order_item_shipped" {
@@ -30,7 +37,7 @@ resource "aws_sqs_queue" "soh_order_item_shipped" {
 }
 
 resource "aws_sqs_queue" "soh_order_payment_secured_dlq" {
-  name = local.sqs_queues.order_payment_secured
+  name = local.sqs_queues.order_payment_secured_dlq
 }
 
 resource "aws_sqs_queue" "soh_order_payment_secured" {
@@ -39,7 +46,7 @@ resource "aws_sqs_queue" "soh_order_payment_secured" {
 }
 
 resource "aws_sqs_queue" "soh_order_item_transmitted_to_logistic_dlq" {
-  name = local.sqs_queues.order_item_transmitted_to_logistic
+  name = local.sqs_queues.order_item_transmitted_to_logistic_dlq
 }
 
 resource "aws_sqs_queue" "soh_order_item_transmitted_to_logistic" {
@@ -48,7 +55,7 @@ resource "aws_sqs_queue" "soh_order_item_transmitted_to_logistic" {
 }
 
 resource "aws_sqs_queue" "soh_order_item_packing_started_dlq" {
-  name = local.sqs_queues.order_item_packing_started
+  name = local.sqs_queues.order_item_packing_started_dlq
 }
 
 resource "aws_sqs_queue" "soh_order_item_packing_started" {
@@ -57,7 +64,7 @@ resource "aws_sqs_queue" "soh_order_item_packing_started" {
 }
 
 resource "aws_sqs_queue" "soh_order_item_tracking_id_received_dlq" {
-  name = local.sqs_queues.order_item_tracking_id_received
+  name = local.sqs_queues.order_item_tracking_id_received_dlq
 }
 
 resource "aws_sqs_queue" "soh_order_item_tracking_id_received" {
@@ -66,7 +73,7 @@ resource "aws_sqs_queue" "soh_order_item_tracking_id_received" {
 }
 
 resource "aws_sqs_queue" "soh_order_item_tour_started_dlq" {
-  name = local.sqs_queues.order_item_tour_started
+  name = local.sqs_queues.order_item_tour_started_dlq
 }
 
 resource "aws_sqs_queue" "soh_order_item_tour_started" {
