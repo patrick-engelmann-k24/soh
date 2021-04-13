c3997001ff6d0f64de4b64d586978901b5b09dba
vinaya.nayak88 <vinaya.nayak88@gmail.com>
2021-04-13 15:16:19 +0200

    DEV-25916 : improve order created event

---
+++ src/main/java/de/kfzteile24/salesOrderHub/dto/MarketingSalesOrder.java
@@ -0,0 +1,20 @@
+package de.kfzteile24.salesOrderHub.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author vinaya
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MarketingSalesOrder {
+
+  private OrderJSON order;
+  private boolean recurringOrder;
+
+}
