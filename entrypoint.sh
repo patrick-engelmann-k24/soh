5e231ce59958f14af2440078e1b15d694ab75a6e
vinaya.nayak88 <vinaya.nayak88@gmail.com>
2021-04-15 13:44:19 +0200

    DEV-25015 : cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ cd /opt/newrelic
 
 if [ -n "${newrelic_license}" ]; then
     sed -i -e "s/'<%= license_key %>'/'${newrelic_license}'/" /opt/newrelic/newrelic.yml
-    sed -i -e 's/app_name: My Application/app_name: SOH-business-processing-engine/' /opt/newrelic/newrelic.yml
+    sed -i -e 's/app_name: My Application/app_name: SOH-Business-Processing-Engine/' /opt/newrelic/newrelic.yml
     java_opts=${java_opts}' -javaagent:/opt/newrelic/newrelic.jar'
     export java_opts
 fi
