81d735354b6f8de765cc6cce65ca7dff1e610dc7
vinaya.nayak88 <vinaya.nayak88@gmail.com>
2021-04-20 15:09:57 +0200

    DEV-25856 : drop sales_order_address & sales_order_item table tables

--- src/test/java/de/kfzteile24/salesOrderHub/helper/SalesOrderUtil.java
+++ src/test/java/de/kfzteile24/salesOrderHub/helper/SalesOrderUtil.java
@@ -7,10 +7,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.google.gson.Gson;
 import de.kfzteile24.salesOrderHub.domain.SalesOrder;
-import de.kfzteile24.salesOrderHub.domain.SalesOrderItem;
 import de.kfzteile24.salesOrderHub.dto.OrderJSON;
 import de.kfzteile24.salesOrderHub.dto.order.LogisticalUnits;
-import de.kfzteile24.salesOrderHub.dto.order.Rows;
 import de.kfzteile24.salesOrderHub.dto.sqs.EcpOrder;
 import de.kfzteile24.salesOrderHub.services.SalesOrderService;
 import lombok.SneakyThrows;
@@ -23,7 +21,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.math.BigDecimal;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -31,7 +28,6 @@ import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
-import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -75,18 +71,6 @@ public class SalesOrderUtil {
         List<LogisticalUnits> logisticalUnits = orderJSON.getLogisticalUnits();
         assertEquals(1, logisticalUnits.size());
 
-        Set<SalesOrderItem> testOrderItems = new HashSet<>();
-
-        List<Rows> orderItems = orderJSON.getOrderRows();
-        orderItems.forEach(row-> {
-            SalesOrderItem salesOrderItem = new SalesOrderItem();
-            salesOrderItem.setQuantity( new BigDecimal(row.getQuantity().toString()));
-            salesOrderItem.setStockKeepingUnit(row.getSku());
-            salesOrderItem.setShippingType(logisticalUnits.get(0).getShippingType());
-            testOrderItems.add(salesOrderItem);
-        });
-
-        testOrder.setSalesOrderItemList(testOrderItems);
         testOrder.setSalesOrderInvoiceList(new HashSet<>());
         salesOrderService.save(testOrder);
         return testOrder;
