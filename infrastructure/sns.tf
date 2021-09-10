32cfe527a4a02663f9c1911a681b446c64ba7eb7
patrick-engelmann-k24 <patrick.engelmann@kfzteile24.de>
2021-09-10 14:47:58 +0200

    commented old topic

--- infrastructure/sns.tf
+++ infrastructure/sns.tf
@@ -59,11 +59,11 @@ data "aws_sns_topic" "sns_braincraft_order_received" {
 }
 
 # subscriptions of sqs to sns
-resource "aws_sns_topic_subscription" "sns_subscription_ecp_orders" {
-  endpoint = aws_sqs_queue.ecp_shop_orders.arn
-  protocol = "sqs"
-  topic_arn = var.ecp_new_order_sns
-}
+//resource "aws_sns_topic_subscription" "sns_subscription_ecp_orders" {
+//  endpoint = aws_sqs_queue.ecp_shop_orders.arn
+//  protocol = "sqs"
+//  topic_arn = var.ecp_new_order_sns
+//}
 
 # subscriptions of sqs to sns
 resource "aws_sns_topic_subscription" "sns_subscription_ecp_orders_v3" {
