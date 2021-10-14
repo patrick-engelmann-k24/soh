adbfc53d325e51155bb8d6f07614d737c941c08a
azat.martirosyan <azat.martirosyan@kfzteile24.de>
2021-10-14 13:46:35 +0200

    DEV-29106: Removed none web context attribute from the tests.

--- src/test/java/de/kfzteile24/salesOrderHub/delegates/salesOrder/ChangeInvoiceAddressPossibleDelegateIntegrationTest.java
+++ src/test/java/de/kfzteile24/salesOrderHub/delegates/salesOrder/ChangeInvoiceAddressPossibleDelegateIntegrationTest.java
@@ -42,8 +42,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @SpringBootTest(
-        classes = SalesOrderHubProcessApplication.class,
-        webEnvironment = SpringBootTest.WebEnvironment.NONE
+        classes = SalesOrderHubProcessApplication.class
 )
 public class ChangeInvoiceAddressPossibleDelegateIntegrationTest {
     @Autowired
