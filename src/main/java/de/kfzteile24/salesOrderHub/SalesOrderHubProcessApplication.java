8c1e16f459a730cb1d43e26ceca33b6f0bbd094a
azat.martirosyan <azat.martirosyan@kfzteile24.de>
2021-12-21 11:03:41 +0100

    DEV-30459: Default pool in spring boot is hikary already. Removed the creation.

--- src/main/java/de/kfzteile24/salesOrderHub/SalesOrderHubProcessApplication.java
+++ src/main/java/de/kfzteile24/salesOrderHub/SalesOrderHubProcessApplication.java
@@ -1,8 +1,13 @@
 package de.kfzteile24.salesOrderHub;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 @SpringBootApplication
 @EnableProcessApplication
@@ -11,4 +16,14 @@ public class SalesOrderHubProcessApplication {
     public static void main(String... args) {
         SpringApplication.run(SalesOrderHubProcessApplication.class, args);
     }
+
+
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource")
+    public HikariDataSource dataSourceSOH() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+
 }
