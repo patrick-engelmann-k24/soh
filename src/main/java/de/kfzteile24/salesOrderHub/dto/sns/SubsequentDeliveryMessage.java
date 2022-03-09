925e90cd53fe603591f43e59817f4eccade0fa81
Samet Nargul <99476300+sametnargulk24@users.noreply.github.com>
2022-03-09 17:35:15 +0300

    DEV-33087 update subsequent delivery json schema (#248)
    * DEV-33087 update subsequent delivery json schema
    
    * DEV-33087 update subsequent delivery json schema

--- src/main/java/de/kfzteile24/salesOrderHub/dto/sns/SubsequentDeliveryMessage.java
+++ src/main/java/de/kfzteile24/salesOrderHub/dto/sns/SubsequentDeliveryMessage.java
@@ -1,5 +1,6 @@
 package de.kfzteile24.salesOrderHub.dto.sns;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import de.kfzteile24.salesOrderHub.dto.sns.subsequent.SubsequentDeliveryItem;
 import lombok.Data;
 
@@ -7,8 +8,13 @@ import java.util.List;
 
 @Data
 public class SubsequentDeliveryMessage {
+
+    @JsonProperty("OrderNumber")
     private String orderNumber;
+    @JsonProperty("SubsequentDeliveryNoteNumber")
     private String subsequentDeliveryNoteNumber;
+    @JsonProperty("CancellationDeliveryNoteNumber")
     private String cancellationDeliveryNoteNumber;
+    @JsonProperty("Items")
     private List<SubsequentDeliveryItem> items;
 }
