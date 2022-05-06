2f01867bf95f36aafda29d2fda28954ee8e761aa
Stefan <stefandragan92@gmail.com>
2022-05-06 21:41:03 +0300

    DEV-33915 Rename localstack sns topic

--- infrastructure/application.tf
+++ infrastructure/application.tf
@@ -46,7 +46,7 @@ module "application_module" {
     sns_soh_return_order_created_v1            = data.aws_sns_topic.sns_soh_return_order_created_v1.arn
     sns_soh_core_invoice_received_v1           = data.aws_sns_topic.sns_soh_core_invoice_received_v1.arn
     sns_soh_credit_note_received_v1            = data.aws_sns_topic.sns_soh_credit_note_received_v1.arn
-    sns_migration_soh_order_create_v2          = data.aws_sns_topic.sns_migration_soh_order_created_v2.arn
+    sns_migration_soh_order_created_v2          = data.aws_sns_topic.sns_migration_soh_order_created_v2.arn
 
     soh_sqs_ecp_shop_orders                    = aws_sqs_queue.ecp_shop_orders.id
     soh_sqs_bc_shop_orders                     = aws_sqs_queue.bc_shop_orders.id
