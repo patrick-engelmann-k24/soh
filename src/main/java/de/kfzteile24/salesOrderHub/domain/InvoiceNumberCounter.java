41ffd55ae46258ef705265fccb6ad4ae51a5b890
Samet Nargul <99476300+sametnargulk24@users.noreply.github.com>
2022-07-25 22:19:47 +0300

    DEV-35909 fix type of year field in invoice number counter table (#556)

--- src/main/java/de/kfzteile24/salesOrderHub/domain/InvoiceNumberCounter.java
+++ src/main/java/de/kfzteile24/salesOrderHub/domain/InvoiceNumberCounter.java
@@ -19,7 +19,7 @@ import javax.persistence.Table;
 public class InvoiceNumberCounter {
 
     @Id
-    @Column(name = "year", columnDefinition = "smallint", updatable = false)
+    @Column(name = "year", columnDefinition = "int", updatable = false)
     private Integer year;
 
     private Long counter;
--- src/main/resources/db/migration/V24__create_invoice_number_counter_table.sql
+++ src/main/resources/db/migration/V24__create_invoice_number_counter_table.sql
@@ -1,6 +1,6 @@
 create table public.invoice_number_counter
 (
-    year                 smallint                         not null,
+    year                 int                              not null,
     counter              bigint                           not null,
     CONSTRAINT pk_invoice_number_counter_year PRIMARY KEY (year)
 );
