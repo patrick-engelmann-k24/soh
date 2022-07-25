4f2e2ef3b04a3cdfb997b414f525cf02b51e250e
mykhailo-skliar-k24 <104762505+mykhailo-skliar-k24@users.noreply.github.com>
2022-07-25 20:06:00 +0200

    feature/DEV-35909-add-postgresql-nextval-for-creating-invoice-numbers (#555)
    * [DEV-35909]
    
    - implemented InvoiceNumberCounter table and correspondent entity, which contains current value of counter for invoice number generation
    - implemented InvoiceNumberCounterService and InvoiceNumberCounterRepository, which return next counter for the current year, using pessimistic write locking and counter increment inside the locked transaction
    
    * DEV-35909 update invoice service and integration tests
    
    * DEV-35909 fix unittests
    
    * DEV-35909 fix codacy
    
    * DEV-35909 fix integration tests
    
    * DEV-35909 rename test
    
    * [DEV-35909]
    
    - implemented lock timeout and transaction propagation level requires new for getNextCounter
    
    * [DEV-35909]
    
    - small fix
    
    * [DEV-35909]
    
    - reduced lock timeout 5 seconds
    
    Co-authored-by: samet nargul <samet.narguel.extern@kfzteile24.de>

--- src/main/java/de/kfzteile24/salesOrderHub/services/InvoiceNumberCounterService.java
+++ src/main/java/de/kfzteile24/salesOrderHub/services/InvoiceNumberCounterService.java
@@ -5,6 +5,7 @@ import de.kfzteile24.salesOrderHub.repositories.InvoiceNumberCounterRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
@@ -12,6 +13,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.LockModeType;
 import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
@@ -38,9 +41,11 @@ public class InvoiceNumberCounterService {
         }
     }
 
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public Long getNextCounter(Integer currentYear) {
-        InvoiceNumberCounter invoiceNumberCounter = entityManager.find(InvoiceNumberCounter.class, currentYear, LockModeType.PESSIMISTIC_WRITE);
+        InvoiceNumberCounter invoiceNumberCounter = entityManager.find(InvoiceNumberCounter.class, currentYear, LockModeType.PESSIMISTIC_WRITE,
+                Collections.singletonMap("javax.persistence.lock.timeout",
+                        TimeUnit.SECONDS.toMillis(5)));
         Long result = invoiceNumberCounter.getCounter();
         invoiceNumberCounter.setCounter(result + 1);
         invoiceNumberCounterRepository.save(invoiceNumberCounter);
