d692381a8ddc5b00e6b3de44ff8b30c5de8d8de4
samet nargul <samet.narguel.extern@kfzteile24.de>
2022-09-12 18:54:07 +0300

    DEV-36503 add an integration test for msg attributes for others (invoice-created, parcel-shipped)

--- src/test/java/de/kfzteile24/salesOrderHub/configuration/LoggingAdviceIntegrationTest.java
+++ src/test/java/de/kfzteile24/salesOrderHub/configuration/LoggingAdviceIntegrationTest.java
@@ -5,6 +5,7 @@ import de.kfzteile24.salesOrderHub.services.sqs.SqsReceiveService;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
@@ -33,19 +34,20 @@ class LoggingAdviceIntegrationTest {
     @SpyBean
     private AmazonSQSAsync amazonSQSAsync;
 
+    @BeforeEach
+    void setUp() {
+        doReturn(null).when(amazonSQSAsync).sendMessage(any());
+    }
+
     @Test
     @DisplayName("Test Logging Advice Message Attribute Property for Ecp Shop Orders")
     void testMessageAttributeForEcpShop(TestInfo testInfo) {
 
         log.info(testInfo.getDisplayName());
-        String rawMessage = getRawMessage();
+        String rawMessage = getRawMessageOfOrderWithoutCustomerNumber();
 
         sqsReceiveService.queueListenerEcpShopOrders(rawMessage, "senderId", 4);
-
-        verify(amazonSQSAsync).sendMessage(argThat(msgReq -> {
-            assertThat(msgReq.getQueueUrl()).contains("soh-ecp-shop-orders-queue-dlq");
-            return true;
-        }));
+        verifyIfMessageIsSendingToDLQ("soh-ecp-shop-orders-queue-dlq");
     }
 
     @Test
@@ -53,14 +55,10 @@ class LoggingAdviceIntegrationTest {
     void testMessageAttributeForBcShop(TestInfo testInfo) {
 
         log.info(testInfo.getDisplayName());
-        String rawMessage = getRawMessage();
+        String rawMessage = getRawMessageOfOrderWithoutCustomerNumber();
 
         sqsReceiveService.queueListenerBcShopOrders(rawMessage, "senderId", 4);
-
-        verify(amazonSQSAsync).sendMessage(argThat(msgReq -> {
-            assertThat(msgReq.getQueueUrl()).contains("soh-bc-shop-orders-queue-dlq");
-            return true;
-        }));
+        verifyIfMessageIsSendingToDLQ("soh-bc-shop-orders-queue-dlq");
     }
 
     @Test
@@ -68,22 +66,45 @@ class LoggingAdviceIntegrationTest {
     void testMessageAttributeForCoreShop(TestInfo testInfo) {
 
         log.info(testInfo.getDisplayName());
-        String rawMessage = getRawMessage();
+        String rawMessage = getRawMessageOfOrderWithoutCustomerNumber();
 
         sqsReceiveService.queueListenerCoreShopOrders(rawMessage, "senderId", 4);
+        verifyIfMessageIsSendingToDLQ("soh-core-shop-orders-queue-dlq");
+    }
 
-        verify(amazonSQSAsync).sendMessage(argThat(msgReq -> {
-            assertThat(msgReq.getQueueUrl()).contains("soh-core-shop-orders-queue-dlq");
-            return true;
-        }));
+    @Test
+    @DisplayName("Test Logging Advice Message Attribute Property for Invoice Created Event")
+    void testMessageAttributeForInvoiceCreatedEvent(TestInfo testInfo) {
+
+        log.info(testInfo.getDisplayName());
+        String rawMessage = readResource("examples/coreSalesInvoiceCreatedOneItem.json");
+
+        sqsReceiveService.queueListenerCoreSalesInvoiceCreated(rawMessage, "senderId", 4);
+        verifyIfMessageIsSendingToDLQ("soh-core-sales-invoice-created-queue-dlq");
+    }
+
+    @Test
+    @DisplayName("Test Logging Advice Message Attribute Property for Parcel Shipped Event")
+    void testMessageAttributeForParcelShippedEvent(TestInfo testInfo) {
+
+        log.info(testInfo.getDisplayName());
+        String rawMessage = readResource("examples/parcelShipped.json");
+
+        sqsReceiveService.queueListenerParcelShipped(rawMessage, "senderId", 4);
+        verifyIfMessageIsSendingToDLQ("soh-parcel-shipped-queue-dlq");
     }
 
     @NotNull
-    private String getRawMessage() {
+    private String getRawMessageOfOrderWithoutCustomerNumber() {
         String rawMessage = readResource("examples/ecpOrderMessageWithTwoRows.json");
-        rawMessage = rawMessage.replace("DEB-000000000", "");
-        doReturn(null).when(amazonSQSAsync).sendMessage(any());
-        return rawMessage;
+        return rawMessage.replace("DEB-000000000", "");
+    }
+
+    private void verifyIfMessageIsSendingToDLQ(String dlqName) {
+        verify(amazonSQSAsync).sendMessage(argThat(msgReq -> {
+            assertThat(msgReq.getQueueUrl()).contains(dlqName);
+            return true;
+        }));
     }
 
     @SneakyThrows({URISyntaxException.class, IOException.class})
