30d3a427367da9fb0095533a842d37b7699e35ee
samet nargul <samet.narguel.extern@kfzteile24.de>
2022-10-05 13:33:59 +0300

    DEV-36854 check logbook works

--- src/main/java/de/kfzteile24/salesOrderHub/configuration/LogbookConfiguration.java
+++ src/main/java/de/kfzteile24/salesOrderHub/configuration/LogbookConfiguration.java
@@ -1,17 +1,21 @@
 package de.kfzteile24.salesOrderHub.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.zalando.logbook.Logbook;
 
 @Configuration
+@Slf4j
 public class LogbookConfiguration {
 
     @Bean
     public Logbook logbook() {
-        return Logbook.builder()
-                .correlationId(new CustomCorrelationId())
-                .build();
+//        return Logbook.builder()
+//                .correlationId(new CustomCorrelationId())
+//                .build();
+        log.info("LogbookConfiguration");
+        return Logbook.create();
     }
 
 }
