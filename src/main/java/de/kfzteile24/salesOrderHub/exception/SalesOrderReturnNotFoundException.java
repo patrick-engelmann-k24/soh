3d52ead1b1b09c5597d28e713490c199f399c77f
samet nargul <samet.narguel.extern@kfzteile24.de>
2022-10-17 13:46:13 +0300

    DEV-37237 update handlePaypalRefundInstructionSuccessful

--- src/main/java/de/kfzteile24/salesOrderHub/exception/SalesOrderReturnNotFoundException.java
+++ src/main/java/de/kfzteile24/salesOrderHub/exception/SalesOrderReturnNotFoundException.java
@@ -9,4 +9,11 @@ public class SalesOrderReturnNotFoundException  extends NotFoundException {
                 "Sales order return not found for the given order number {0} ", orderNumber));
     }
 
+    public SalesOrderReturnNotFoundException(String orderNumber, String creditNoteNumber) {
+        super(MessageFormat.format(
+                "Sales order return not found for the given order number {0} and credit note number {1} ",
+                orderNumber,
+                creditNoteNumber));
+    }
+
 }
