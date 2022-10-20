1b32b7ab9cd3008fbcca57601f6ebfdfca454693
samet nargul <samet.narguel.extern@kfzteile24.de>
2022-10-20 10:25:21 +0300

    DEV-37204 msg attributes for D365-payment-secured DLQ

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -72,7 +72,8 @@ data "aws_iam_policy_document" "sns_sqs_access_policy" {
       aws_sqs_queue.soh_parcel_shipped_dlq.arn,
       aws_sqs_queue.soh_invoices_from_core_dlq.arn,
       aws_sqs_queue.soh_core_sales_credit_note_created_dlq.arn,
-      aws_sqs_queue.soh_paypal_refund_instruction_successful_dlq.arn
+      aws_sqs_queue.soh_paypal_refund_instruction_successful_dlq.arn,
+      aws_sqs_queue.d365_order_payment_secured_dlq.arn
     ]
   }
   statement {
