7569bac3cc399c7ff76f0c13b52c0cc5e5afdb1f
mykhailo-skliar-k24 <mykhailo.skliar@kfzteile24.de>
2022-11-01 10:14:08 +0100

    fix/DEV-37600-soh-bpmn-credit-notes-for-return-orders-have-null-order-group-id (#707)
    * [DEV-37600]
    
    - implemented testHandleSalesOrderReturn in SalesOrderReturnServiceTest
    - fixed orderGroupId for SalesCreditNoteCreatedMessage by setting orderGroupId from original salesOrder, instead of using correspondent field of SalesCreditNoteCreatedMessage
    
    * [DEV-37600]
    
    - small fix
    
    * [DEV-37600]
    
    - implemented testCreateCreditNoteEventMessage instead of testHandleSalesOrderReturn

--- src/test/java/de/kfzteile24/salesOrderHub/services/SalesOrderReturnServiceTest.java
+++ src/test/java/de/kfzteile24/salesOrderHub/services/SalesOrderReturnServiceTest.java
@@ -1,10 +1,12 @@
 package de.kfzteile24.salesOrderHub.services;
 
 import de.kfzteile24.salesOrderHub.delegates.helper.CamundaHelper;
+import de.kfzteile24.salesOrderHub.dto.sns.SalesCreditNoteCreatedMessage;
 import de.kfzteile24.salesOrderHub.helper.OrderUtil;
 import de.kfzteile24.salesOrderHub.repositories.AuditLogRepository;
 import de.kfzteile24.salesOrderHub.repositories.SalesOrderReturnRepository;
 import de.kfzteile24.salesOrderHub.services.financialdocuments.CreditNoteNumberCounterService;
+import de.kfzteile24.soh.order.dto.Order;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,7 +15,17 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Collections;
+import java.util.Optional;
+
+import static de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.Messages.CORE_CREDIT_NOTE_CREATED;
+import static de.kfzteile24.salesOrderHub.domain.audit.Action.RETURN_ORDER_CREATED;
+import static de.kfzteile24.salesOrderHub.helper.JsonTestUtil.getObjectByResource;
+import static de.kfzteile24.salesOrderHub.helper.SalesOrderUtil.getSalesOrder;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -57,5 +69,21 @@ class SalesOrderReturnServiceTest {
         var currentYear = 2022;
         when(creditNoteNumberCounterService.getNextCounter(currentYear)).thenReturn(2L);
         assertThat(salesOrderReturnService.createCreditNoteNumber(currentYear)).isEqualTo("2022200002");
+    }
+
+    @Test
+    @SneakyThrows
+    @DisplayName(("Test createCreditNoteEventMessage method"))
+    public void testCreateCreditNoteEventMessage() {
+        var order = getObjectByResource("ecpOrderMessage.json", Order.class);
+        order.getOrderHeader().setOrderGroupId(order.getOrderHeader().getOrderNumber());
+        var salesOrder =  getSalesOrder(order);
+        var orderNumber = order.getOrderHeader().getOrderNumber();
+        var orderGroupId = order.getOrderHeader().getOrderGroupId();
+        var message = getObjectByResource("coreSalesCreditNoteCreated.json", SalesCreditNoteCreatedMessage.class);
+        message.getSalesCreditNote().getSalesCreditNoteHeader().setOrderNumber(orderNumber);
+        message.getSalesCreditNote().getSalesCreditNoteHeader().setOrderGroupId(null);
+        var salesCreditNoteCreatedMessage = salesOrderReturnService.createCreditNoteEventMessage(salesOrder, message, orderNumber);
+        assertEquals(orderGroupId, salesCreditNoteCreatedMessage.getSalesCreditNote().getSalesCreditNoteHeader().getOrderGroupId());
     }
 }
