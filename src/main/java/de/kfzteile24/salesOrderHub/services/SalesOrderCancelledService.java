0da2a392d76e57e62b30e795ad46e6530c5cfe3d
Andreas Hoffmann <andreas.hoffmann.extern@kfzteile24.de>
2022-11-08 12:34:56 +0100

    DEV-37620: re-design order cancellation

--- src/main/java/de/kfzteile24/salesOrderHub/services/SalesOrderCancelledService.java
+++ src/main/java/de/kfzteile24/salesOrderHub/services/SalesOrderCancelledService.java
@@ -1,11 +1,17 @@
 package de.kfzteile24.salesOrderHub.services;
 
+import de.kfzteile24.salesOrderHub.delegates.helper.CamundaHelper;
+import de.kfzteile24.salesOrderHub.domain.SalesOrder;
 import de.kfzteile24.salesOrderHub.dto.sns.CoreSalesOrderCancelledMessage;
 import de.kfzteile24.salesOrderHub.services.sqs.EnrichMessageForDlq;
 import de.kfzteile24.salesOrderHub.services.sqs.MessageWrapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.variable.Variables;
 import org.springframework.stereotype.Service;
+
+import static de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.Messages.ORDER_CANCELLATION_RECEIVED;
+import static de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.Variables.ORDER_NUMBER;
 
 @Service
 @Slf4j
@@ -14,6 +20,7 @@ public class SalesOrderCancelledService {
 
     private final SalesOrderService salesOrderService;
     private final SnsPublishService snsPublishService;
+    private final CamundaHelper camundaHelper;
 
     private final SalesOrderRowService salesOrderRowService;
 
@@ -21,7 +28,10 @@ public class SalesOrderCancelledService {
     public void handleCoreSalesOrderCancelled(CoreSalesOrderCancelledMessage message, MessageWrapper messageWrapper) {
         final var orderNumber = message.getOrderNumber();
         log.info("Received core sales order cancelled message with order number: {}", orderNumber);
-        var salesOrder = salesOrderRowService.cancelOrder(orderNumber);
-        snsPublishService.publishOrderCancelled(salesOrder.getLatestJson());
+        SalesOrder salesOrder = null; // get sales order from DB
+        camundaHelper.correlateMessage(ORDER_CANCELLATION_RECEIVED, salesOrder,
+                Variables.putValue(ORDER_NUMBER.getName(), orderNumber)); // start event subprocess interrupting the main process at any point
+        // var salesOrder = salesOrderRowService.cancelOrder(orderNumber); goes to the new delegate to be created
+        // snsPublishService.publishOrderCancelled(salesOrder.getLatestJson()); not required -> see OrderCancelledDelegate
     }
 }
