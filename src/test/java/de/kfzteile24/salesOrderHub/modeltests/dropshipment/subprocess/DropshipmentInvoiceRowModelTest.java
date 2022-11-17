6a8ed397bec851a7c726eacb14c1c680f8bd1c42
mykhailo.skliar <mykhailo.skliar@kfzteile24.de>
2022-11-17 09:08:29 +0100

    [DEV-37719]
    - continue implementation of model test

--- src/test/java/de/kfzteile24/salesOrderHub/modeltests/dropshipment/subprocess/DropshipmentInvoiceRowModelTest.java
+++ src/test/java/de/kfzteile24/salesOrderHub/modeltests/dropshipment/subprocess/DropshipmentInvoiceRowModelTest.java
@@ -16,20 +16,13 @@ import static de.kfzteile24.salesOrderHub.constants.bpmn.ProcessDefinition.SALES
 import static de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.Activities.DROPSHIPMENT_INVOICE_ROW_CREATE_INVOICE_ENTRY;
 import static de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.Activities.DROPSHIPMENT_INVOICE_ROW_PROCESS;
 import static de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.Activities.DROPSHIPMENT_INVOICE_ROW_PUBLISH_TRACKING_INFORMATION;
-import static de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.Activities.DROPSHIPMENT_ORDER_ROWS_CANCELLATION;
 import static de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.Activities.EVENT_MSG_DROPSHIPMENT_INVOICE_ROW_TRACKING_INFORMATION_RECEIVED;
-import static de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.Activities.EVENT_MSG_DROPSHIPMENT_ORDER_CONFIRMED;
-import static de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.Activities.EVENT_MSG_DROPSHIPMENT_ORDER_TRACKING_INFORMATION_RECEIVED;
-import static de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.Activities.EVENT_THROW_MSG_PURCHASE_ORDER_SUCCESSFUL;
 import static de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.CustomerType.NEW;
-import static de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.Events.END_MSG_ORDER_CANCELLED;
-import static de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.Events.THROW_MSG_DROPSHIPMENT_ORDER_CREATED;
 import static de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.Gateways.XOR_CHECK_DROPSHIPMENT_ORDER_SUCCESSFUL;
 import static de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.Variables.IS_DROPSHIPMENT_ORDER_CONFIRMED;
 import static de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.row.PaymentType.CREDIT_CARD;
 import static de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.row.ShipmentMethod.REGULAR;
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -58,8 +51,10 @@ class DropshipmentInvoiceRowModelTest extends AbstractWorkflowTest {
 
         processVariables.put(IS_DROPSHIPMENT_ORDER_CONFIRMED.getName(), true);
 
-        when(processScenario.waitsAtReceiveTask(EVENT_MSG_DROPSHIPMENT_INVOICE_ROW_TRACKING_INFORMATION_RECEIVED.getName()))
-                .thenReturn(receiverTask -> receiverTask.defer(DEFER_PERIOD_SECONDS_25, receiverTask::receive));
+        when(processScenario.waitsAtMessageIntermediateCatchEvent(EVENT_MSG_DROPSHIPMENT_INVOICE_ROW_TRACKING_INFORMATION_RECEIVED.getName()))
+                .thenReturn(WAIT_MESSAGE_CATCH_EVENT_ACTION);
+        when(processScenario.runsCallActivity(DROPSHIPMENT_INVOICE_ROW_PROCESS.getName()))
+                .thenReturn(executeCallActivity());
 
         scenario = startBeforeActivity(SALES_ORDER_PROCESS, XOR_CHECK_DROPSHIPMENT_ORDER_SUCCESSFUL.getName(),
                 businessKey, processVariables);
@@ -71,4 +66,29 @@ class DropshipmentInvoiceRowModelTest extends AbstractWorkflowTest {
         assertThat(scenario.instance(processScenario)).isEnded();
 
     }
+
+    /*@Test
+    @Tags(@Tag("IsBranchOrderGatewayTest"))
+    @DisplayName("Start process before gwXORCheckBranchType. isBranchOrder is false")
+    void testIsBranchOrderGatewayTestisBranchOrderFalse(TestInfo testinfo) {
+        log.info("{} - {}", testinfo.getDisplayName(), testinfo.getTags());
+
+        processVariables.put(IS_BRANCH_ORDER.getName(), false);
+        processVariables.put(POSITIVE_PAYMENT_TYPE.getName(), true);
+
+        when(processScenario.waitsAtMessageIntermediateCatchEvent(MSG_ORDER_PAYMENT_SECURED.getName()))
+                .thenReturn(RECEIVED_MESSAGE_CATCH_EVENT_ACTION);
+        when(processScenario.waitsAtMessageIntermediateCatchEvent(ROW_TRANSMITTED_TO_LOGISTICS.getName()))
+                .thenReturn(WAIT_MESSAGE_CATCH_EVENT_ACTION);
+        when(processScenario.runsCallActivity(ORDER_ROW_FULFILLMENT_PROCESS.getName()))
+                .thenReturn(executeCallActivity());
+
+        scenario = startBeforeActivity(SALES_ORDER_PROCESS, XOR_CHECK_BRANCH_TYPE.getName(),
+                businessKey, processVariables);
+
+        verify(processScenario).hasCompleted(XOR_CHECK_BRANCH_TYPE.getName());
+        verify(processScenario).hasCompleted(XOR_CHECK_PAYMENT_TYPE.getName());
+        verify(processScenario, times(3)).waitsAtMockedCallActivity(ORDER_ROW_FULFILLMENT_PROCESS.getName());
+        verify(processScenario, times(3)).waitsAtMessageIntermediateCatchEvent(ROW_TRANSMITTED_TO_LOGISTICS.getName());
+    }*/
 }
