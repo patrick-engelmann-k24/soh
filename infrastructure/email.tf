d0757086fd4f5591b8de55d710b2190e6010f150
Stefan <stefandragan92@gmail.com>
2022-11-22 13:48:20 +0200

    DEV-37850 Remove external emails from terraform files

--- infrastructure/email.tf
+++ infrastructure/email.tf
@@ -1,11 +1,3 @@
 resource "aws_ses_email_identity" "soh_source_email" {
   email = aws_ssm_parameter.soh_source_email.value
 }
-
-resource "aws_ses_email_identity" "finance_destination_email" {
-  email = aws_ssm_parameter.finance_destination_email.value
-}
-
-resource "aws_ses_email_identity" "refund_cc_email" {
-  email = aws_ssm_parameter.refund_cc_email.value
-}
