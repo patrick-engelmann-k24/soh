dce40dc0f0ec0cee2dd7749dcfe4f3563bf59a95
jin804 <hyeonjin.lee@kfzteile24.de>
2022-11-23 11:21:47 +0100

    DEV-37779 invoice aggregation

--- src/main/java/de/kfzteile24/salesOrderHub/delegates/dropshipmentorder/listener/IsPartialInvoiceDelegate.java
+++ src/main/java/de/kfzteile24/salesOrderHub/delegates/dropshipmentorder/listener/IsPartialInvoiceDelegate.java
@@ -1,20 +1,20 @@
-package de.kfzteile24.salesOrderHub.delegates.dropshipmentorder;
+package de.kfzteile24.salesOrderHub.delegates.dropshipmentorder.listener;
 
 import de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.Variables;
 import de.kfzteile24.salesOrderHub.domain.bpmn.orderProcess.InvoiceDataVariable;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
-import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.delegate.ExecutionListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
-public class IsPartialInvoiceDelegate implements JavaDelegate {
+public class IsPartialInvoiceDelegate implements ExecutionListener {
 
     @Override
-    public void execute(DelegateExecution delegateExecution) throws Exception {
+    public void notify(DelegateExecution delegateExecution) throws Exception {
 
         InvoiceDataVariable invoiceData =
                 (InvoiceDataVariable) delegateExecution.getVariable(Variables.INVOICE_DATA.getName());
