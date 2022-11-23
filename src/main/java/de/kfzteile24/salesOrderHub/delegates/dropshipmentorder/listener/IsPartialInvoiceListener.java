4a7947d9b254a3ff6f663f64a9cfa28bb9f14e72
samet nargul <samet.narguel.extern@kfzteile24.de>
2022-11-23 17:59:54 +0300

    DEV-37974 fix IsPartialInvoiceListener

--- src/main/java/de/kfzteile24/salesOrderHub/delegates/dropshipmentorder/listener/IsPartialInvoiceListener.java
+++ src/main/java/de/kfzteile24/salesOrderHub/delegates/dropshipmentorder/listener/IsPartialInvoiceListener.java
@@ -1,7 +1,9 @@
 package de.kfzteile24.salesOrderHub.delegates.dropshipmentorder.listener;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.Variables;
 import de.kfzteile24.salesOrderHub.domain.bpmn.orderProcess.InvoiceDataVariable;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
@@ -13,12 +15,16 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class IsPartialInvoiceListener implements ExecutionListener {
 
+    @NonNull
+    private final ObjectMapper objectMapper;
+
     @Override
     public void notify(DelegateExecution delegateExecution) throws Exception {
 
-        InvoiceDataVariable invoiceData =
-                (InvoiceDataVariable) delegateExecution.getVariable(Variables.INVOICE_DATA.getName());
+        String invoiceDataVariable =
+                (String) delegateExecution.getVariable(Variables.INVOICE_DATA.getName());
 
+        var invoiceData = objectMapper.readValue(invoiceDataVariable, InvoiceDataVariable.class);
         var isPartialInvoice = invoiceData.isPartialInvoice();
 
         delegateExecution.setVariable(Variables.IS_PARTIAL_INVOICE.getName(), isPartialInvoice);
