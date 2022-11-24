767c0c435284feca1c632575b1da4f263c8a4ba5
samet nargul <samet.narguel.extern@kfzteile24.de>
2022-11-24 18:03:44 +0300

    DEV-37779 create invoice_number index

--- src/main/resources/db/migration/V31__create_idx_order_number_for_ds_invoice_row.sql
+++
@@ -1,4 +0,0 @@
--- Add order_number index if not already exists
-CREATE INDEX IF NOT EXISTS order_numberx ON public.dropshipment_invoice_row (order_number);
-
-COMMENT ON INDEX order_numberx IS 'An index on order number';
