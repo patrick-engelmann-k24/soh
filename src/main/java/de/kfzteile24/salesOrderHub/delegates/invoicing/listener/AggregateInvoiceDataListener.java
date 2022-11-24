ece7e72cd0226ed55202c8f2f910a15eae1a4227
samet nargul <samet.narguel.extern@kfzteile24.de>
2022-11-24 11:13:55 +0300

    DEV-37974 enrich content of AggregateInvoiceDataListener

--- src/main/java/de/kfzteile24/salesOrderHub/delegates/invoicing/listener/AggregateInvoiceDataListener.java
+++ src/main/java/de/kfzteile24/salesOrderHub/delegates/invoicing/listener/AggregateInvoiceDataListener.java
@@ -1,5 +1,8 @@
 package de.kfzteile24.salesOrderHub.delegates.invoicing.listener;
 
+import de.kfzteile24.salesOrderHub.services.dropshipment.DropshipmentInvoiceRowService;
+import de.kfzteile24.salesOrderHub.services.financialdocuments.InvoiceService;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
@@ -8,6 +11,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.Variables.INVOICE_NUMBER_LIST;
 
@@ -15,10 +19,27 @@ import static de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.Variables.
 @Component
 @RequiredArgsConstructor
 public class AggregateInvoiceDataListener implements ExecutionListener {
+    @NonNull
+    private final InvoiceService invoiceService;
+    @NonNull
+    private final DropshipmentInvoiceRowService dropshipmentInvoiceRowService;
 
     @Override
     @Transactional
     public void notify(DelegateExecution delegateExecution) throws Exception {
-        delegateExecution.setVariable(INVOICE_NUMBER_LIST.getName(), new ArrayList<>());
+        log.info("Daily aggregation of invoice data has started.");
+
+        var invoiceDataMap = dropshipmentInvoiceRowService.generateInvoiceDataMap(
+                dropshipmentInvoiceRowService.findAllOrderByOrderNumberAsc());
+
+        List<String> invoiceNumberList = new ArrayList<>();
+        for (var orderNumber : invoiceDataMap.keySet()) {
+
+            var invoiceNumber = invoiceService.createInvoiceNumber();
+            dropshipmentInvoiceRowService.saveInvoiceNumber(orderNumber, invoiceNumber);
+            invoiceNumberList.add(invoiceNumber);
+        }
+
+        delegateExecution.setVariable(INVOICE_NUMBER_LIST.getName(), invoiceNumberList);
     }
 }
