8541024770f1e4c05ab2b09843b767c973ed28cd
mykhailo.skliar <mykhailo.skliar@kfzteile24.de>
2022-12-13 15:38:19 +0100

    [DEV-38400]
    - fixed AggregationInvoiceDataModelTest after merge

--- src/test/java/de/kfzteile24/salesOrderHub/modeltests/invoice/AggregationInvoiceDataModelTest.java
+++ src/test/java/de/kfzteile24/salesOrderHub/modeltests/invoice/AggregationInvoiceDataModelTest.java
@@ -1,7 +1,9 @@
 package de.kfzteile24.salesOrderHub.modeltests.invoice;
 
+import de.kfzteile24.salesOrderHub.delegates.invoicing.AggregateInvoiceDataDelegate;
 import de.kfzteile24.salesOrderHub.helper.SalesOrderUtil;
 import de.kfzteile24.salesOrderHub.modeltests.AbstractWorkflowTest;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +11,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
@@ -25,6 +28,7 @@ import static de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.Variables.
 import static de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.row.PaymentType.CREDIT_CARD;
 import static de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.row.ShipmentMethod.REGULAR;
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -32,6 +36,9 @@ import static org.mockito.Mockito.verify;
 @DisplayName("AggregationInvoiceData model test")
 @Slf4j(topic = "AggregationInvoiceData model test")
 class AggregationInvoiceDataModelTest extends AbstractWorkflowTest {
+
+    @Autowired
+    AggregateInvoiceDataDelegate aggregateInvoiceDataDelegate;
 
     @BeforeEach
     protected void setUp() {
@@ -44,6 +51,7 @@ class AggregationInvoiceDataModelTest extends AbstractWorkflowTest {
     @Test
     @Tags(@Tag("InvoiceDataAggregationTriggeredAndSubprocessesStartedTest"))
     @DisplayName("After the timer is triggered, start aggregation of invoice data and subprocesses.")
+    @SneakyThrows
     void testInvoiceDataAggregationTriggeredAndSubprocessesStarted(TestInfo testinfo) {
         log.info("{} - {}", testinfo.getDisplayName(), testinfo.getTags());
 
@@ -53,6 +61,7 @@ class AggregationInvoiceDataModelTest extends AbstractWorkflowTest {
         scenario = startBeforeActivity(INVOICING_PROCESS, AGGREGATE_INVOICE_DATA.getName(),
                 businessKey, processVariables);
 
+        verify(aggregateInvoiceDataDelegate).execute(any());
         verify(processScenario, times(1)).hasCompleted(AGGREGATE_INVOICE_DATA.getName());
         verify(processScenario, times(3)).hasCompleted("eventStartSubInvoicing");
 
@@ -63,6 +72,7 @@ class AggregationInvoiceDataModelTest extends AbstractWorkflowTest {
     @Test
     @Tags(@Tag("ActivityCreateDropshipmentSalesOrderInvoiceStartedForPartialInvoiceFalseTest"))
     @DisplayName("If isPartialInvoice false, start activityCreateDropshipmentSalesOrderInvoice.")
+    @SneakyThrows
     void testActivityCreateDropshipmentSalesOrderInvoiceStartedForPartialInvoiceFalse(TestInfo testinfo) {
         log.info("{} - {}", testinfo.getDisplayName(), testinfo.getTags());
 
@@ -72,6 +82,7 @@ class AggregationInvoiceDataModelTest extends AbstractWorkflowTest {
         scenario = startBeforeActivity(INVOICING_PROCESS, DETERMINE_DROPSHIPMENT_ORDER_INVOICE_TYPE.getName(),
                 businessKey, processVariables);
 
+        verify(aggregateInvoiceDataDelegate).execute(any());
         verify(processScenario, times(1)).hasCompleted(DETERMINE_DROPSHIPMENT_ORDER_INVOICE_TYPE.getName());
         verify(processScenario, times(1)).hasCompleted(XOR_CHECK_PARTIAL_INVOICE.getName());
         verify(processScenario, times(1)).hasCompleted(INVOICING_CREATE_DROPSHIPMENT_SALES_ORDER_INVOICE.getName());
@@ -84,6 +95,7 @@ class AggregationInvoiceDataModelTest extends AbstractWorkflowTest {
     @Test
     @Tags(@Tag("ActivityCreateInvoiceSubsequentOrderStartedForPartialInvoiceTrueTest"))
     @DisplayName("If isPartialInvoice true, start activityCreateInvoiceSubsequentOrder.")
+    @SneakyThrows
     void testActivityCreateInvoiceSubsequentOrderStartedForPartialInvoiceTrue(TestInfo testinfo) {
         log.info("{} - {}", testinfo.getDisplayName(), testinfo.getTags());
 
@@ -93,6 +105,7 @@ class AggregationInvoiceDataModelTest extends AbstractWorkflowTest {
         scenario = startBeforeActivity(INVOICING_PROCESS, DETERMINE_DROPSHIPMENT_ORDER_INVOICE_TYPE.getName(),
                 businessKey, processVariables);
 
+        verify(aggregateInvoiceDataDelegate).execute(any());
         verify(processScenario, times(1)).hasCompleted(DETERMINE_DROPSHIPMENT_ORDER_INVOICE_TYPE.getName());
         verify(processScenario, times(1)).hasCompleted(XOR_CHECK_PARTIAL_INVOICE.getName());
         verify(processScenario, never()).hasCompleted(INVOICING_CREATE_DROPSHIPMENT_SALES_ORDER_INVOICE.getName());
