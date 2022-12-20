fe89e9d44fbdd9c3f8e8f3eb88be7b8dc334dc34
mykhailo.skliar <mykhailo.skliar@kfzteile24.de>
2022-12-20 16:36:09 +0100

    [DEV-38528]
    - fixed junit and integrationt test (small fixes)

--- src/test/java/de/kfzteile24/salesOrderHub/delegates/salesOrder/CancelDropshipmentOrderDelegateTest.java
+++ src/test/java/de/kfzteile24/salesOrderHub/delegates/salesOrder/CancelDropshipmentOrderDelegateTest.java
@@ -2,6 +2,9 @@ package de.kfzteile24.salesOrderHub.delegates.salesOrder;
 
 import de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.Variables;
 import de.kfzteile24.salesOrderHub.delegates.helper.CamundaHelper;
+import de.kfzteile24.salesOrderHub.domain.SalesOrder;
+import de.kfzteile24.salesOrderHub.helper.MetricsHelper;
+import de.kfzteile24.salesOrderHub.services.SalesOrderService;
 import lombok.SneakyThrows;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.junit.jupiter.api.Test;
@@ -9,6 +12,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -21,6 +26,10 @@ public class CancelDropshipmentOrderDelegateTest {
 
     @Mock
     private CamundaHelper camundaHelper;
+    @Mock
+    private MetricsHelper metricsHelper;
+    @Mock
+    private SalesOrderService salesOrderService;
 
     @InjectMocks
     private CancelDropshipmentOrderDelegate cancelDropshipmentOrderDelegate;
@@ -30,6 +39,7 @@ public class CancelDropshipmentOrderDelegateTest {
     void theDelegateCancelsOrder() {
         final var expectedOrderNumber = "123";
         when(delegateExecution.getVariable(Variables.ORDER_NUMBER.getName())).thenReturn(expectedOrderNumber);
+        when(salesOrderService.getOrderByOrderNumber(expectedOrderNumber)).thenReturn(Optional.of(SalesOrder.builder().build()));
         cancelDropshipmentOrderDelegate.execute(delegateExecution);
         verify(camundaHelper).correlateDropshipmentOrderCancelledMessage(expectedOrderNumber);
     }
