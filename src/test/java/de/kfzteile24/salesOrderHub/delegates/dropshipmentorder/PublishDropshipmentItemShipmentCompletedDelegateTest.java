4df6491095d06724f2bf5173075d3390b52e6d55
samet nargul <samet.narguel.extern@kfzteile24.de>
2023-01-12 12:30:00 +0300

    DEV-38695 fix correlation between dropshipment-shipment-process and main-process

--- src/test/java/de/kfzteile24/salesOrderHub/delegates/dropshipmentorder/PublishDropshipmentItemShipmentCompletedDelegateTest.java
+++ src/test/java/de/kfzteile24/salesOrderHub/delegates/dropshipmentorder/PublishDropshipmentItemShipmentCompletedDelegateTest.java
@@ -1,9 +1,10 @@
 package de.kfzteile24.salesOrderHub.delegates.dropshipmentorder;
 
 import de.kfzteile24.salesOrderHub.delegates.helper.CamundaHelper;
+import de.kfzteile24.salesOrderHub.domain.SalesOrder;
+import de.kfzteile24.salesOrderHub.services.SalesOrderService;
 import lombok.SneakyThrows;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
-import org.camunda.bpm.extension.mockito.process.ProcessInstanceFake;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Answers;
@@ -11,9 +12,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.Messages.DROPSHIPMENT_ORDER_FULLY_COMPLETED;
 import static de.kfzteile24.salesOrderHub.constants.bpmn.orderProcess.Variables.ORDER_NUMBER;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -23,11 +25,10 @@ class PublishDropshipmentItemShipmentCompletedDelegateTest {
 
     @Mock
     private DelegateExecution delegateExecution;
-
     @Mock(answer = Answers.RETURNS_DEEP_STUBS)
     private CamundaHelper camundaHelper;
-
-
+    @Mock
+    private SalesOrderService salesOrderService;
     @InjectMocks
     private PublishDropshipmentItemShipmentCompletedDelegate publishDropshipmentItemShipmentCompletedDelegate;
 
@@ -35,10 +36,10 @@ class PublishDropshipmentItemShipmentCompletedDelegateTest {
     @SneakyThrows(Exception.class)
     void testDropshipmentOrderFullyShippedDelegate() {
         final var orderNumber = "123456789";
+        final var salesOrder = SalesOrder.builder().build();
         when(delegateExecution.getVariable(ORDER_NUMBER.getName())).thenReturn(orderNumber);
-        when(camundaHelper.correlateMessage(any(), anyString()).getProcessInstance())
-                .thenReturn(ProcessInstanceFake.builder().build());
+        when(salesOrderService.getOrderByOrderNumber(anyString())).thenReturn(Optional.ofNullable(salesOrder));
         publishDropshipmentItemShipmentCompletedDelegate.execute(delegateExecution);
-        verify(camundaHelper).correlateMessage(DROPSHIPMENT_ORDER_FULLY_COMPLETED, orderNumber);
+        verify(camundaHelper).correlateMessage(DROPSHIPMENT_ORDER_FULLY_COMPLETED, salesOrder);
     }
 }
