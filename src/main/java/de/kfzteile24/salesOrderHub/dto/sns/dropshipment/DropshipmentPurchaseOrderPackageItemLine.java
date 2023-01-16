475d354fa26868b8f02afa70a4feb561bbe07330
Stefan <stefandragan92@gmail.com>
2023-01-16 14:22:17 +0200

    DEV-38876 Add validations to Dropshipment return confirmed

--- src/main/java/de/kfzteile24/salesOrderHub/dto/sns/dropshipment/DropshipmentPurchaseOrderPackageItemLine.java
+++ src/main/java/de/kfzteile24/salesOrderHub/dto/sns/dropshipment/DropshipmentPurchaseOrderPackageItemLine.java
@@ -6,15 +6,19 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class DropshipmentPurchaseOrderPackageItemLine {
 
+    @NotNull
     @JsonProperty("ProductNumber")
     private String productNumber;
 
+    @NotNull
     @JsonProperty("Quantity")
     private Integer quantity;
 }
